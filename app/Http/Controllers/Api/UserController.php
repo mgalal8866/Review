6fc426c2b84643bd6b2f5d73f6e77628eb5b42ee
Mohamed Galal <mgalal8866@gmail.com>
2024-12-06 01:16:42 +0200

    modified:   app/Http/Controllers/Api/UserController.php

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
     public function login(Request $request)
     {
 
-        $creadebtions = $request->vaildate([
+        $creadebtions = $request->validate([
             'email' => ['required'],
             'password' => ['required']
         ]);
