96f03ca7e9ca8c90795c99f1affc6a9eccbb8816
Mohamed Galal <mgalal8866@gmail.com>
2024-12-06 01:13:49 +0200

    modified:   routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\ReviewController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::post('/login', [UserController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/reviews/{type}/{id}', [ReviewController::class, 'store']);
